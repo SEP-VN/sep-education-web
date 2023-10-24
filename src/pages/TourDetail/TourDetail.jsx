0abb0d5d58cda8eb8e0f89eaa73443ce04e27c62
Tran Anh Duy NGUYEN <anhduydp123@gmail.com>
2023-10-24 14:16:53 +0200

    update tour detail page

--- src/pages/TourDetail/TourDetail.jsx
+++ src/pages/TourDetail/TourDetail.jsx
@@ -4,16 +4,16 @@ import './TourDetailStyle.css'
 const TourDetail = () => {
   return (
     <div class="container">
-<div class="row">
+<div class="row mt-4">
   <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
     <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+      src="https://oms.hotdeal.vn/images/editors/sources/000333275524/333275-333275-body%20(10).jpg"
       class="w-100 shadow-1-strong rounded mb-4"
       alt="Boat on Calm Water"
     />
 
     <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWpQYycaUu1vqgIF-z8KnGguDE9BwGcPdAqg&usqp=CAU"
       class="w-100 shadow-1-strong rounded mb-4"
       alt="Wintry Mountain Landscape"
     />
@@ -21,13 +21,13 @@ const TourDetail = () => {
 
   <div class="col-lg-4 mb-4 mb-lg-0">
     <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT__693esE4EzXKv-UKS357E0Q2ztHysPjFug&usqp=CAU"
       class="w-100 shadow-1-strong rounded mb-4"
       alt="Mountains in the Clouds"
     />
 
     <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+      src="https://oms.hotdeal.vn/images/editors/sources/000333275524/333275-333275-body%20(10).jpg"
       class="w-100 shadow-1-strong rounded mb-4"
       alt="Boat on Calm Water"
     />
@@ -35,29 +35,30 @@ const TourDetail = () => {
 
   <div class="col-lg-4 mb-4 mb-lg-0">
     <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+      src="https://oms.hotdeal.vn/images/editors/sources/000333275524/333275-333275-body%20(10).jpg"
       class="w-100 shadow-1-strong rounded mb-4"
       alt="Waves at Sea"
     />
 
     <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWpQYycaUu1vqgIF-z8KnGguDE9BwGcPdAqg&usqp=CAU"
       class="w-100 shadow-1-strong rounded mb-4"
       alt="Yosemite National Park"
     />
   </div>
 </div>
-<div className="row mt-4">
-<h2 className="text-center mb-3">Mục tiêu của dự án</h2>
+<hr class="mx-auto mb-5 mb-xl-9 border-dark-subtle"/>
+<div className="row mt-5">
+<h2 className="fw-bold mb-3">Mục tiêu của dự án</h2>
   <p>
-  Du lịch tình nguyện tại Bảo Lộc – Lâm Đồng nhằm hỗ trợ, thúc đẩy phát triển mô hình du lịch cộng đồng tại nơi đây và quảng bá các nghề truyền thống địa phương. Với mục đích phát triển sinh kế cho địa phương đi liền quảng bá du lịch nơi đây. Dự án mang lại cho địa phương cách khai thác các nguồn lực về du lịch còn chưa được khai thác. 
+  Du lịch tình nguyện tại làng Tà Lài nhằm hỗ trợ phát triển giáo dục, thúc đẩy phát triển mô hình du lịch cộng đồng tại nơi đây và quảng bá các nghề truyền thống địa phương. Với mục đích phát triển sinh kế cho địa phương đi liền quảng bá du lịch nơi đây. Dự án mang lại cho địa phương cách khai thác các nguồn lực về du lịch còn chưa được khai thác. 
 
-Thông qua các hoạt động hỗ trợ thiết thực như đào tạo kỹ năng việc làm, dạy học phi chính quy, quảng bá, truyền thông về hình ảnh du lịch cộng đồng tại địa phương, tổ chức các chương trình du lịch trải nghiệm, V.E.O mong muốn mang lại những cơ hội thiết thực nhất, phát triển kinh tế bền vững nhất cho bà con nơi đây. 
+Thông qua các hoạt động hỗ trợ thiết thực như đào tạo kỹ năng việc làm, dạy học phi chính quy, quảng bá, truyền thông về hình ảnh du lịch cộng đồng tại địa phương, tổ chức các chương trình du lịch trải nghiệm, S.E.P mong muốn mang lại những cơ hội thiết thực nhất, phát triển kinh tế bền vững nhất cho bà con nơi đây. 
   </p>
 </div>
-
+<hr class="mx-auto mb-5 mb-xl-9 border-dark-subtle"/>
 <div className="row mt-4">
-<h2 className="text-center mb-4">Thông tin của chuyến đi</h2>
+<h2 className="fw-bold mb-3">Thông tin của chuyến đi</h2>
   <div className="row">
     <div className="col-md-6">
       <h4>Điểm nổi bật của chuyến đi</h4>
@@ -77,9 +78,9 @@ Thông qua các hoạt động hỗ trợ thiết thực như đào tạo kỹ n
     </div>
   </div>
 </div>
-
+<hr class="mx-auto mb-5 mb-xl-9 border-dark-subtle"/>
 <div className="row mt-4">
-<h2 className="text-center mb-4">Các môn giảng dạy dự kiến</h2>
+<h2 className="fw-bold mb-3">Các môn giảng dạy dự kiến</h2>
   <ol class="list-group list-group-numbered">
   <li class="list-group-item d-flex justify-content-between align-items-start">
     <div class="ms-2 me-auto">
@@ -100,12 +101,138 @@ Thông qua các hoạt động hỗ trợ thiết thực như đào tạo kỹ n
     </div>
   </li>
 </ol> </div>
-
+<hr class="mx-auto mb-5 mb-xl-9 border-dark-subtle"/>
 <div className="row mt-5">
-<h2 className="text-center mb-3">Chi phí</h2>
+<h2 className="fw-bold">Lịch trình</h2>
+<div class="container-fluid py-9 py-lg-11 position-relative z-index-1">
+    <div class="row">
+            <div class="col-lg-4 py-4 mb-lg-0">
+             
+                <div class="nav nav-pills flex-column aos-init aos-animate" id="myTab" role="tablist" data-aos="fade-up">
+                    <button class="btn-schedule-tour nav-link px-4 text-start mb-3 active" id="d1-tab" data-bs-toggle="tab" data-bs-target="#day1"
+                     type="button" role="tab" aria-controls="day1" aria-selected="true">
+                        <span class="d-block fs-5 fw-bold">Ngày 1</span>
+                        <span class="small">T2 - 23/10/2023</span>
+                    </button>
+                    
+                    <button class="nav-link px-4 text-start" id="d2-tab" data-bs-toggle="tab" data-bs-target="#day2" type="button" role="tab" aria-controls="day2" aria-selected="false" tabindex="-1">
+                        <span class="d-block fs-5 fw-bold">Ngày 2</span>
+                        <span class="small">T3 - 24/10/2023</span>
+                    </button>
+                </div>
+            </div>
+            
+            <div class="col-lg-8 col-xl-8">
+                <div data-aos="fade-up" class="tab-content aos-init aos-animate" id="myTabContent">
+                    <div class="tab-pane fade active show" id="day1" role="tabpanel" aria-labelledby="d1-tab">
+                        <ul class="pt-4 list-unstyled mb-0">
+                            <li class="d-flex flex-column flex-md-row py-4">
+                                <span class="flex-shrink-0 width-13x me-md-4 d-block mb-3 mb-md-0 small text-muted">9:00
+                                    AM - 10:00 AM</span>
+                                <div class="flex-grow-1 ps-4 border-start border-3">
+                                    <h5>Khởi hành, ăn sáng trên xe</h5>
+                                  
+                                </div>
+                            </li>
+                            <li class="d-flex flex-column flex-md-row py-4">
+                                <span class="flex-shrink-0 width-13x me-md-4 d-block mb-3 mb-md-0 small text-muted">10:00
+                                    AM - 11:00 AM</span>
+                                <div class="flex-grow-1 ps-4 border-start border-3">
+                                    <h5>Tới Tà Lài, nhận phòng</h5>
+                                    
+                                </div>
+                            </li>
+                            <li class="d-flex flex-column flex-md-row py-4">
+                                <span class="flex-shrink-0 width-13x me-md-4 d-block mb-3 mb-md-0 small text-muted">11:00
+                                    AM - 12:30 PM</span>
+                                <div class="flex-grow-1 ps-4 border-start border-3">
+                                    <h5>Họp BTC và TNV về lịch trình giảng dạy</h5>
+                                
+                                </div>
+                            </li>
+                            <li class="d-flex flex-column flex-md-row py-4">
+                                <span class="flex-shrink-0 width-13x me-md-4 d-block mb-3 mb-md-0 small text-muted">12:30
+                                    PM - 1:30 PM</span>
+                                <div class="flex-grow-1 ps-4 border-start border-3">
+                                    <h5>Tham quan nhà truyền thống trong làng / Tham quan làng (đi bộ vào làng, tham quan đập tràn, khu vực giảng dạy)</h5>
+                                  
+                                </div>
+                            </li>
+                            <li class="d-flex flex-column flex-md-row py-4">
+                                <span class="flex-shrink-0 width-13x me-md-4 d-block mb-3 mb-md-0 small text-muted">12:30
+                                    PM - 3:30 PM</span>
+                                <div class="flex-grow-1 ps-4 border-start border-3">
+                                    <h5>Đi tham quan cánh đồng vườn dầu (di chuyển 1h30’, tham quan 30’)</h5>
+                                  
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="tab-pane fade" id="day2" role="tabpanel" aria-labelledby="d2-tab">
+                        <ul class="pt-4 list-unstyled mb-0">
+                        <li class="d-flex flex-column flex-md-row py-4">
+                                <span class="flex-shrink-0 width-13x me-md-4 d-block mb-3 mb-md-0 small text-muted">9:00
+                                    AM - 10:00 AM</span>
+                                <div class="flex-grow-1 ps-4 border-start border-3">
+                                    <h5>Khởi hành, ăn sáng trên xe</h5>
+                                  
+                                </div>
+                            </li>
+                            <li class="d-flex flex-column flex-md-row py-4">
+                                <span class="flex-shrink-0 width-13x me-md-4 d-block mb-3 mb-md-0 small text-muted">10:00
+                                    AM - 11:00 AM</span>
+                                <div class="flex-grow-1 ps-4 border-start border-3">
+                                    <h5>Tới Tà Lài, nhận phòng</h5>
+                                    
+                                </div>
+                            </li>
+                            <li class="d-flex flex-column flex-md-row py-4">
+                                <span class="flex-shrink-0 width-13x me-md-4 d-block mb-3 mb-md-0 small text-muted">11:00
+                                    AM - 12:30 PM</span>
+                                <div class="flex-grow-1 ps-4 border-start border-3">
+                                    <h5>Họp BTC và TNV về lịch trình giảng dạy</h5>
+                                
+                                </div>
+                            </li>
+                            <li class="d-flex flex-column flex-md-row py-4">
+                                <span class="flex-shrink-0 width-13x me-md-4 d-block mb-3 mb-md-0 small text-muted">12:30
+                                    PM - 1:30 PM</span>
+                                <div class="flex-grow-1 ps-4 border-start border-3">
+                                    <h5>Tham quan nhà truyền thống trong làng / Tham quan làng (đi bộ vào làng, tham quan đập tràn, khu vực giảng dạy)</h5>
+                                  
+                                </div>
+                            </li>
+                            <li class="d-flex flex-column flex-md-row py-4">
+                                <span class="flex-shrink-0 width-13x me-md-4 d-block mb-3 mb-md-0 small text-muted">12:30
+                                    PM - 3:30 PM</span>
+                                <div class="flex-grow-1 ps-4 border-start border-3">
+                                    <h5>Đi tham quan cánh đồng vườn dầu (di chuyển 1h30’, tham quan 30’)</h5>
+                                  
+                                </div>
+                            </li>
+              
+                       
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr class="mx-auto mb-5 mb-xl-9 border-dark-subtle"/>
+<div className="row mt-5">
+<h2 className="fw-bold mb-3">Chi phí</h2>
   <p>
-    
+    - Chi phí tổng : <span className="fw-bold" style={{color: "#ffb03b"}}> 3.990.000 VND</span>
   </p>
+  <p>
+    - Chi phí bao gồm :
+  </p>
+    <ul class="px-5" style={{listStyleType:"disc"}}>
+    <li>Tiền xe di chuyển đến địa điểm</li>
+    <li>Tiền ăn uống trong chuyến đi</li>
+    <li>Tiền tham gia tour của S.E.P</li>
+</ul>
 </div>
   
 
